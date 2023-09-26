85862065d25a93e645c7bb5d59bbab2ed6dbf3c2
Josue Mejia <josueomejiap@gmail.com>
2023-09-25 20:08:24 -0600

    trabajo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+  titulo = 'Registro de Usuarios';
+  mensaje = '';
+  registrado = false;
+  nombre = '';
+  apellido = '';
+  cargo = '';
+
+  entradas:any[];
+
+  constructor(){
+    this.entradas = [
+      {
+        titulo: "Python, cada día más presente"
+      },
+      {
+        titulo: "Java, presente más de 20 años"
+      },
+      {
+        titulo: "JavaScript, cada día más funcional"
+      }
+    ];
+  }
+
+  registrar_usuario(){
+    this.registrado = true;
+    this.mensaje = 'Usuario registrado exitosamente';
+  }
 }
